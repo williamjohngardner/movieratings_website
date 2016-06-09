b45a98d58b9e7544bc482747b142eb49d259b37c
williamjohngardner <bill@williamjohngardner.com>
2016-06-09 15:07:28 -0400

    Database Loaded

--- movieratings/admin.py
+++ movieratings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from movieratings.models import Rating
+from movieratings.models import Rater
+from movieratings.models import Movie
 
-# Register your models here.
+admin.site.register([Rater, Rating, Movie])
